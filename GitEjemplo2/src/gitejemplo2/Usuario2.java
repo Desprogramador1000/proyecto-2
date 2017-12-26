0e041068a31d5f2f78429d4d139d82ce458d389a
Desprogramador1000 <novoaperez@gmail.com>
2017-12-26 20:04:22 -0300

    primer commit inicial
    confirmando proyecto primera vez

---
+++ GitEjemplo2/src/gitejemplo2/Usuario2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitejemplo2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Usuario2 {
+    String nombre;
+
+    public Usuario2() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
